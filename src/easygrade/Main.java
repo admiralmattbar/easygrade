747c410bbe48363cf7733d9a477f5e7bec5875c9
admiralmattbar <mdkissel@gmail.com>
2018-09-26 20:18:49 -0700

    First Commit

---
+++ src/easygrade/Main.java
@@ -0,0 +1,25 @@
+/**
+ * Easy Grade
+ * An small, simple application to keep track of student grades.
+ * Create and save lists of students and assignments as well as weighted grades.
+ *
+ * (c) 2018 Matthew Kissel
+ * GitHub https://github.com/admiralmattbar/easygrade.git
+ */
+
+package easygrade;
+
+import easygrade.util.ConsoleTime;
+
+public class Main {
+
+    public static long start_time;
+
+    public static void main(String args[]) throws InterruptedException {
+        start_time = System.currentTimeMillis();
+        ConsoleTime.printLog("Starting Program.");
+        
+    }
+
+
+}
